d3a481f22b905c1cfb41633a5911dca3a861fd74
wenxueliu <wenxueliu73@163.com>
2018-05-08 08:38:20 +0800

    init commit

---
+++ src/main/java/com/dapp/ShudongApplication.java
@@ -0,0 +1,12 @@
+package com.dapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShudongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShudongApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dapp/ShudongApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShudongApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
